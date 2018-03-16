6fc3b8e65429d3a0055216eb4d4f259ef5e82a8a
bater <jace_ka@yahoo.com.tw>
2018-03-16 13:10:33 +1100

    create basic user model

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  validates :name, :email, :password_digest, presence: true
+  validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+
+  has_secure_password
+end
